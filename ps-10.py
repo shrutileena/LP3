108cf7355f349e6aa344c3a45103f71c9a2cf627
shrutileena <shrutileena@gmail.com>
2020-06-08 07:24:44 +0530

    Assignment-10 committed

---
+++ ps-10.py
@@ -0,0 +1,12 @@
+class node:
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def traverse(self, data):
+
+
+a = list(map(int, input("Enter BST elements with space in between: ").strip().split(' ')))
+n = node()
+n.search(a)
