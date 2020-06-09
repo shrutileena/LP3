a95268c7dbad986a37093f11a70cc87952248ca1
shrutileena <shrutileena@gmail.com>
2020-06-09 11:54:41 +0530

    Assingment-10 committed

--- ps-10.py
+++ ps-10.py
@@ -1,12 +1,36 @@
 class node:
     def __init__(self, data):
         self.data = data
-        self.left = None
-        self.right = None
+        self.left_child = None
+        self.right_child = None
 
-def traverse(self, data):
+def isBst(root, left, right):
+    if root == None:
+        return True
 
+    if (left != None and root.data <= left.data):
+        return False
 
-a = list(map(int, input("Enter BST elements with space in between: ").strip().split(' ')))
-n = node()
-n.search(a)
+    if (right != None and root.data >= right.data):
+        return False
+
+    return isBst(root.left_child, left, root) and isBst(root.right_child, root, right)
+
+if __name__ == '__main__':
+    root = node(2)
+    root.left_child = node(1)
+    root.right_child = node(3)
+
+    if (isBst(root,None,None)):
+        print("Is BST")
+    else:
+        print("Not a BST")
+
+    root = node(1)
+    root.left_child = node(2)
+    root.right_child = node(3)
+
+    if (isBst(root,None,None)):
+        print("Is BST")
+    else:
+        print("Not a BST")
